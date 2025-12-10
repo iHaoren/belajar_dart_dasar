3cedaf3e9c01d8e6aef5d12b6d8dd30e0b76d167
Ibnu Harun <ibnuharunalmudzakir@gmail.com>
2025-12-10 20:46:39 +0700

    belajar dart dasar

---
+++ bin/operator_penugasan.dart
@@ -0,0 +1,18 @@
+void main() {
+  // Operator Penugasan
+
+  var a = 10;
+
+  // a = a + 10;
+  a += 10;
+
+  print(a);
+
+  //Increment dan Decrement
+  var i = 0;
+
+  var j = i++;
+
+  print(i);
+  print(j);
+}
