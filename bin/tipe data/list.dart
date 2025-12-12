32ae695439ad6afbef18bcf8a3d13ede956a42e5
Ibnu Harun <ibnuharunalmudzakir@gmail.com>
2025-12-12 20:24:23 +0700

    belajar dart

--- bin/tipe data/list.dart
+++ bin/tipe data/list.dart
@@ -5,10 +5,9 @@ void main(){
   //  saat kita membuat list, kita perlu menentukan isi dari tipe data lis
   //  untuk membuat <data List, kita bisa menggunakan []
   //  di dart, semua tipe data adalah project, dimana list sendiri memiliki      property, method dan operator
-  
 
+  var list = ['Car', 'Boat', 'Plane'];
 
-   
-
+  print(list);
 
 }
